b418500825610f5229e6b99b3220e14df069c673
Martin Ludwig <ML.Commits@gmail.com>
2023-05-10 23:03:04 +0200

    added conversation loop. added colors

--- Shelby.CLI/Program.cs
+++ Shelby.CLI/Program.cs
@@ -1,22 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using OpenAI_API.Chat;
 
+Console.ForegroundColor = ConsoleColor.DarkGray;
 Console.WriteLine("Shelby by Ludwig");
 Console.WriteLine("~");
 Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Yellow;
 Console.Write("Your Open AI Key = ");
+Console.ForegroundColor = ConsoleColor.Gray;
 string? openaiApiKey = Console.ReadLine();
-
-
-//List<Character> Players = new()
-//{
-//    new Character("Alice", 100, 10),
-//    new Character("Bob", 100, 7)
-//};
-
-//var c = new Combat(Players);
-
-//c.Battle();
 
 var api = new OpenAI_API.OpenAIAPI(openaiApiKey);
 
@@ -24,11 +17,15 @@ Conversation? chat = default;
 
 if (await api.Auth.ValidateAPIKey())
 {
+    Console.ForegroundColor = ConsoleColor.Yellow;
     Console.WriteLine("~ OK\n");
     chat = api.Chat.CreateConversation();
 } else
 {
+    Console.ForegroundColor = ConsoleColor.Yellow;
     Console.WriteLine("~ Api Key is not valid.\n");
+
+    Console.ForegroundColor = ConsoleColor.Gray;
     Console.WriteLine("To get an OpenAI API key, you will need to create an account on the OpenAI website https://openai.com/.");
     Console.WriteLine("1. Log in to your OpenAI account on their website.");
     Console.WriteLine("2. Navigate to the \"API\" section.");
@@ -38,20 +35,52 @@ if (await api.Auth.ValidateAPIKey())
     Console.WriteLine("6. Copy your API key and store it in a safe place.");
     Console.WriteLine("7. Use your API key to authenticate your API requests to OpenAI.");
 
-    Console.WriteLine("\n~end");
+    Console.ForegroundColor = ConsoleColor.DarkGray;
+    Console.WriteLine("\n~ end");
     Console.ReadLine();
     Environment.Exit(1);
 }
 
-Console.WriteLine("You > ");
-string userInput = Console.ReadLine();
-Console.WriteLine("");
+while (true)
+{
+    // Read user input
+    if (chat.Messages.Count < 3)
+    {
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.WriteLine("You > ");
+    }
 
-chat.AppendUserInput(userInput);
+    Console.ForegroundColor = ConsoleColor.White;
+    string? userInput = Console.ReadLine();
+    Console.WriteLine("");
 
-string response = await chat.GetResponseFromChatbotAsync();
-Console.WriteLine($"GPT > ");
-Console.WriteLine($"{response}");
+    if (userInput == "exit")
+    {
+        break;
+    }
 
-Console.WriteLine("\n~end");
+    chat.AppendUserInput(userInput);
+
+    // Write response
+    await GptSaysAsync();
+}
+
+Console.ForegroundColor = ConsoleColor.DarkGray;
+Console.WriteLine("\n~ end");
 Console.ReadLine();
+
+async Task GptSaysAsync()
+{
+    if (chat.Messages.Count < 2)
+    {
+        Console.ForegroundColor = ConsoleColor.DarkCyan;
+        Console.WriteLine($"GPT > ");
+    }
+
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    await foreach (var res in chat.StreamResponseEnumerableFromChatbotAsync())
+    {
+        Console.Write(res);
+    }
+    Console.WriteLine("\n");
+}
